62ea00b60a27c7730a44855c244f4e9ffdb0d6f9
Marcos000007 <marcosantony.asp@gmail.com>
2024-04-15 17:08:25 -0300

    15-04-24-inicio

---
+++ site/includes/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/footer.css">
+</head>
+
+<!-- Footer -->
+<footer>
+    <br>
+    <p>&copy;<?php echo date("Y");?> - Todos os direitos reservados.</p>
+</footer>
+
+</html>
